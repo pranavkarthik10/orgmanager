ec45a3ffe0d78bdd99f71dbf7f1063facdf3cab0
pranavkarthik10 <pranav.karthik@icloud.com>
2020-01-14 21:14:52 -0800

    Remove mail

--- app/Http/Controllers/JoinController.php
+++ app/Http/Controllers/JoinController.php
@@ -1,7 +1,5 @@
 <?php
-
 namespace App\Http\Controllers;
-
 use Github;
 use App\Org;
 use Socialite;
@@ -10,53 +8,42 @@ use Illuminate\Http\Request;
 use App\Http\Requests\JoinOrgRequest;
 use Illuminate\Support\Facades\Artisan;
 use Laravel\Socialite\Two\InvalidStateException;
-
 class JoinController extends Controller
 {
     use CaptchaTrait;
-
     public function index(Org $org)
     {
         return view('join')->with('org', $org);
     }
-
     public function inviteUser(JoinOrgRequest $request, Org $org)
     {
         $validation = $this->validateRequest($request, $org);
         if ($validation) {
             return $validation;
         }
-
-        return Socialite::driver('github')->setScopes([])->redirect();
+        return Socialite::driver('github')->setScopes([])->redirectUrl(route('join.callback', $org))->redirect();
     }
-
     public function callback(Request $request, Org $org)
     {
         try {
             $user = Socialite::driver('github')->user();
         } catch (InvalidStateException $e) {
-            return redirect('callback')->withErrors('Something went wrong when authenticating with GitHub. Please try again later or open an issue.');
+            return redirect('join/'.$org->id)->withErrors('Something went wrong when authenticating with GitHub. Please try again later or open an issue.');
         }
         if ($this->isMember($org, $user = $user->getNickname())) {
-            return redirect('callback');
+            return redirect('join/'.$org->id)->withErrors(trans('alerts.member'));
         }
-
         Artisan::call('orgmanager:joinorg', [
           'org'      => $org->id,
           'username' => $user,
-          'email'    => $email,
       ]);
-
         return redirect(url("https://github.com/orgs/$org->name/invitation/"));
     }
-
     public function redirect($name)
     {
         $org = Org::where('name', $name)->firstOrFail();
-
-        return redirect('callback');
+        return redirect('join/'.$org->id);
     }
-
     protected function isMember(Org $org, $username)
     {
         Github::authenticate($org->user->token, null, 'http_token');
@@ -65,21 +52,19 @@ class JoinController extends Controller
         } catch (Github\Exception\RuntimeException $e) {
             return false;
         }
-
         return true;
     }
-
     protected function validateRequest(Request $request, Org $org)
     {
         if (! $this->captchaCheck($request)) {
-            return redirect('callback')->withErrors('You need to prove you are not a robot!');
+            return redirect('join/'.$org->id)->withErrors('You need to prove you are not a robot!');
         }
         if ($org->password && trim($org->password) != '') {
             if (! $request->has('org_password')) {
-                return redirect('callback')->withErrors(trans('alerts.passwd1'));
+                return redirect('join/'.$org->id)->withErrors(trans('alerts.passwd1'));
             }
             if (! password_verify($request->org_password, $org->password)) {
-                return redirect('callback')->withErrors(trans('alerts.passwd2'));
+                return redirect('join/'.$org->id)->withErrors(trans('alerts.passwd2'));
             }
         }
     }
